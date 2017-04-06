31f59337a90c54a71c858b8aea104d2e556c311c
kevin yu <bbcyyb@gmail.com>
2017-04-05 21:03:11 -0700

    Implement crawlAndParse method for each html page

--- src/main/scala/Crawler2.scala
+++ src/main/scala/Crawler2.scala
@@ -1,15 +1,49 @@
 package org.kevin.app.bookcrawler
 
+import scala.collection.JavaConversions._
+import scala.collection.mutable
+import java.net.URL
+import org.jsoup.Jsoup
+
 class Crawler2 {
-    def crawl(basicUrl: String): (String, String) => {
 
+    private val protocol = "http://"
+
+    private def getHostBase(url: String) = {
+        val uri = new URL(url)
+        val portPart = if(uri.getPort() == -1 || uri.getPort() == 80) "" else ":" + uri.getPort()
+        uri.getProtocol() + "://" + uri.getHost() + portPart
     }
 
-    def parse() = {
+    def crawlAndParse(basicUrl: String, linkFilter: (String => Boolean) = (url: String) => true): (String, String, List[String]) = {
 
+        val html = Jsoup.connect(basicUrl).get()
+
+        val c = html.select("div.chapter").first
+        var title = ""
+        var content = ""
+        if(c != null) {
+            val title = c.select("h1").first.text
+            val content = c.select("div").first.html
+        }
+        val alinks = html.select("a[href]").map(_.attr("href")).filter(
+            link => !link.startsWith("javascript:")
+            ).map(
+            link =>
+            link match {
+                case link if link.startsWith("/") => getHostBase(basicUrl) + link
+                case link if link.startsWith("http://") || link.startsWith("https://") => link
+                case _ => link
+            }
+            ).filter(linkFilter).toList
+        
+
+        //.map(_.attr("href")).toList
+
+        return (title, "content", alinks)
     }
 
-    def store() = {
+    def store = {
 
     }
 }
--- src/main/scala/NovelFinder.scala
+++ src/main/scala/NovelFinder.scala
@@ -2,10 +2,11 @@ package org.kevin.app.bookcrawler
 
 object MovelFinder {
   def main(args: Array[String]): Unit = {
-    //val url = "http://www.7caimi.com/xiaoshuo/13/"
-    //val crawler = new Crawler(url, filter = uri => uri.contains(url))
-    //crawler.crawl()
+    val crawler = new Crawler2
+    val basicUrl = "http://www.7caimi.com/xiaoshuo/13/"
+    val result = crawler.crawlAndParse(basicUrl, f => f.contains(basicUrl))
+    println(result)
     
-    new SevenCaiMi(Set("大主宰")).process
+    //new SevenCaiMi(Set("大主宰")).process
   }
 }
