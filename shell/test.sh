176028d8ad2c4e3c67f6603ace49f5ea082a1067
kevin yu <bbcyyb@gmail.com>
2017-05-06 09:34:35 +0800

    Add count shell.

--- shell/test.sh
+++ shell/test.sh
@@ -1,2 +1,63 @@
 #!/bin/bash
 echo "start to run test.sh script"
+
+FILENAME="$1"
+TIMEFILE="/tmp/loopfile.out" > $TIMEFILE
+SCRIPT=$(basename $0)
+
+function usage(){
+    echo -e "\nUSAGE: ${SCRIPT} file \n"
+    exit 1
+}
+
+function while_read_bottom(){
+    while read LINE
+    do
+        echo -n ${LINE}
+    done < $FILENAME
+}
+
+function while_read_line(){
+    cat $FILENAME | while read LINE
+    do
+        echo $LINE
+    done
+}
+
+function while_read_fd(){
+    exec 3<&0
+    exec 0<$FILENAME
+    while read LINE
+    do
+        echo $LINE
+    done
+    exec 0<&3
+}
+
+function for_in_file(){
+    for i in `cat ${FILENAME}`
+    do
+        echo $i
+    done
+}
+
+function countLines(){
+    exp="第\s*[0-9]+\s*章"
+    
+    while read LINE
+    do
+        result=$(echo ${LINE} | grep -P -o "${exp}")
+        if [[ -n $result ]]; then
+            echo "$result"
+        fi
+        
+    done < $FILENAME
+}
+
+if [[ $# -lt 1 ]]; then
+    usage
+fi
+
+countLines
+
+echo -e "\n start file processing of each method\n"
